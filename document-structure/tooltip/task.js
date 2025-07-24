62e06fe3f2eb06f520926d11754c0ff74a2661b9
Tanya <an5227952@gmail.com>
2025-07-24 12:35:27 +0300

    новые подсказки

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,21 +1,41 @@
-const hasTooltips = Array.from(document.querySelectorAll(".has-tooltip"));
-const tooltip = document.createElement("div");
-tooltip.classList.add("tooltip");
+const links = document.getElementsByClassName('has-tooltip');
 
-for (let i = 0; i < hasTooltips.length; i++) {
-    hasTooltips[i].insertAdjacentElement('afterEnd', tooltip);
-    const onClick = (e) => {
+for (const link of links) {
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    tooltip.innerText = link.title;    
+    link.insertAdjacentElement('afterEnd', tooltip);
+
+    link.addEventListener('click', function(e) {
         e.preventDefault();
-        const top = e.target.getBoundingClientRect().top;
-        const left = e.target.getBoundingClientRect().left;
-        if (tooltip.innerText === e.target.title) {
-            tooltip.classList.toggle("tooltip_active");
-        } else {
-            tooltip.classList.add("tooltip_active");
-            tooltip.innerText = e.target.title;
-            tooltip.style.top = `${top + 20}px`; 
-            tooltip.style.left = `${left}px`;
-        } 
-    }
-    hasTooltips[i].addEventListener('click', onClick);
+        if (tooltip.classList.contains('tooltip_active')) {
+            tooltip.classList.remove('tooltip_active');
+            return;
+          }
+    const prevTooltip = document.querySelector('.tooltip_active');
+    if (prevTooltip)
+      prevTooltip.classList.remove('tooltip_active');
+    link.removeAttribute("title");
+        
+    tooltip.classList.add('tooltip_active');
+    const too = tooltip.getBoundingClientRect();
+    const lin = link.getBoundingClientRect();
+    switch (e.target.tooltip) {
+        case 'left':
+          tooltip.style.top = lin.top + 'px';
+          tooltip.style.left = (lin.left + too.left - too.right) + 'px';
+          break;
+        case 'right':
+          tooltip.style.top = lin.top + 'px';
+          tooltip.style.left = lin.right + 'px';        
+          break;
+        case 'top':
+          tooltip.style.top = (lin.top + too.top - too.bottom) + 'px';
+          tooltip.style.left = lin.left + 'px';            
+          break;
+        default:
+          tooltip.style.top = lin.bottom + 'px';
+          tooltip.style.left = lin.left + 'px'; 
+    }    
+    })   
 }
